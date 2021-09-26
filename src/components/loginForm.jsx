f4e7feccb25cfaff7fb7b40ce9685c30f923071e
Kyle Hammond <kyle.hammond@gmail.com>
2021-09-26 17:57:29 -0500

    handle submit, prevent default

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,23 +1,29 @@
 import React, { Component } from "react";
 
 class LoginForm extends React.Component {
+	handleSubmit = (e) => {
+		e.preventDefault();
+
+		// Call the server
+
+		console.log("Submited");
+	};
+
 	render() {
 		return (
 			<div>
 				<h1>Login</h1>
-				<form action="">
+				<form onSubmit={this.handleSubmit}>
 					<div className="form-group">
 						<label htmlFor="username">Username</label>
 						<input id="username" type="text" className="form-control" />
 					</div>
-				</form>
-				<form action="">
 					<div className="form-group">
 						<label htmlFor="password">Password</label>
 						<input id="password" type="text" className="form-control" />
 					</div>
+					<button className="btn btn-primary">Login</button>
 				</form>
-				<button className="btn btn-primary">Login</button>
 			</div>
 		);
 	}
