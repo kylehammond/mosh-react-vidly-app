36b61ef3ea5d1b5366023701a3532967c2ce8643
Kyle Hammond <kyle.hammond@gmail.com>
2021-09-26 17:48:41 -0500

    basic login form built

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,7 +2,24 @@ import React, { Component } from "react";
 
 class LoginForm extends React.Component {
 	render() {
-		return <h1></h1>;
+		return (
+			<div>
+				<h1>Login</h1>
+				<form action="">
+					<div className="form-group">
+						<label htmlFor="username">Username</label>
+						<input id="username" type="text" className="form-control" />
+					</div>
+				</form>
+				<form action="">
+					<div className="form-group">
+						<label htmlFor="password">Password</label>
+						<input id="password" type="text" className="form-control" />
+					</div>
+				</form>
+				<button className="btn btn-primary">Login</button>
+			</div>
+		);
 	}
 }
 
