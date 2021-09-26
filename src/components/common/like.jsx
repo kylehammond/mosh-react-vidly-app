5a920451020e6bf5ec3b5c0293959db4e5c80a2c
Kyle Hammond <kyle.hammond@gmail.com>
2021-09-25 21:58:36 -0500

    fixed problem with props

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,39 +1,15 @@
 import React, { Component } from "react";
 
-class Like extends Component {
-	getLikedClass = () => {
-		const { liked } = this.props;
-		const baseClasses = "fa fa-heart";
-		return liked ? baseClasses : `${baseClasses}-o`;
-	};
-
-	render() {
-		// const { liked, onClick } = this.props;
-		return (
-			<i
-				onClick={this.props.onClick}
-				style={{ cursor: "pointer" }}
-				className={this.getLikedClass()}
-				aria-hidden="true"></i>
-		);
-	}
-}
+const Like = (props) => {
+	let classes = "fa fa-heart";
+	if (!props.liked) classes += "-o";
+	return (
+		<i
+			onClick={props.onClick}
+			style={{ cursor: "pointer" }}
+			className={classes}
+			area-hidden="true"></i>
+	);
+};
 
 export default Like;
-
-// this threw errors for me but it's what he did
-// import React, { Component } from "react";
-
-// const Like = () => {
-// 	let classes = "fa fa-heart";
-// 	if (!this.props.liked) classes += "-o";
-// 	return (
-// 		<i
-// 			onClick={this.props.onClick}
-// 			style={{ cursor: "pointer" }}
-// 			className={classes}
-// 			area-hidden="true"></i>
-// 	);
-// };
-
-// export default Like;
