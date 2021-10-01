ad4ad9ea2c8429c0f344e94dfbca1e263759a303
Kyle Hammond <kyle.hammond@gmail.com>
2021-09-30 19:56:06 -0500

    fixed error messgae

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -12,7 +12,7 @@ const Input = ({ name, label, value, error, onChange }) => {
 				type="text"
 				className="form-control"
 			/>
-			{error && <div className="alert alert-danger">error</div>}
+			{error && <div className="alert alert-danger">{error}</div>}
 		</div>
 	);
 };
